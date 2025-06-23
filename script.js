79f0e06a405ec80943851bb606f7bab2fba87ced
MARYAMTEE <ayorindemaryam078@gmail.com>
2025-06-23 23:43:08 +0100

    update style.css

---
+++ script.js
@@ -0,0 +1,7 @@
+const localGovt = document.querySelector(".local-gvt");
+const dropdownMenu = document.querySelector(".dropdown-menu");
+
+localGovt.addEventListener("click", ()=> {
+    
+    dropdownMenu.classList.toggle("toggle");
+})
